b133603fb389a5ce2a4542282241173167f78fed
Kshitij Singh Chauhan <kshitijsinghchauhan@gmail.com>
2023-12-20 10:30:55 +0530

    Image Smoother

---
+++ DailyLeet/Dec2023/19Dec23.swift
@@ -0,0 +1,43 @@
+//
+//  19Dec23.swift
+//  DailyLeet
+//
+//  Created by Kshitij Singh Chauhan on 19/12/23.
+//
+
+import Foundation
+
+class ImageSmoother {
+    func imageSmoother(_ img: [[Int]]) -> [[Int]] {
+        let m = img.count
+        let n = img[0].count
+        var arr = Array(repeating: Array(repeating: 0, count: n), count: m)
+        for i in 0..<m {
+            for j in 0..<n {
+                let cells = getCells(row: i, column: j, m: m, n: n)
+                arr[i][j] = average(arr: img, cells: cells)
+            }
+        }
+        return arr
+    }
+    
+    private func getCells(row: Int, column: Int, m: Int, n: Int) -> [[Int]] {
+        var cells: [[Int]] = [
+            [row-1, column-1], [row-1, column], [row-1, column+1],
+            [row, column-1], [row, column], [row, column+1],
+            [row+1, column-1], [row+1, column], [row+1, column+1]
+        ]
+        
+        return cells.filter({
+            ($0[0] >= 0 && $0[0] < m) && ($0[1] >= 0 && $0[1] < n)
+        })
+    }
+    
+    private func average(arr: [[Int]], cells: [[Int]]) -> Int {
+        var count: Int = 0
+        for cell in cells {
+            count += arr[cell[0]][cell[1]]
+        }
+        return Int(floor(Double(count / cells.count)))
+    }
+}
